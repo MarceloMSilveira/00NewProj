5bb41076ddfe89c4a4fd3dcb8401b51fccf47f2e
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2024-11-04 17:03:04 -0300

    userTsErasers01

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length)
 
 @Component({
   selector: 'app-user',
@@ -12,14 +9,8 @@ const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length)
 })
 
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex]
 
-  get imagePath(){
-    return './assets/users/'+this.selectedUser.avatar
+   onSelectUser(){
   }
-
-  onSelectUser(){
-    const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];
-  }
+  
 }
