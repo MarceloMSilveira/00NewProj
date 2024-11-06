59ca3d2bbab163b14186731be0457d8a39391124
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2024-11-06 10:12:27 -0300

    using output function

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, input, Input, Output } from '@angular/core';
+import { Component, EventEmitter, input, Input, Output, output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -13,8 +13,8 @@ export class UserComponent {
   @Input({required:true}) avatar!: string;
   @Input({required:true}) name!: string;
 
-  @Output() select = new EventEmitter();
-
+  //@Output() select = new EventEmitter();
+  select = output<string>();
   
   get imagePath() : string {
     return 'assets/users/'+this.avatar;
